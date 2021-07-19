8168e005ad13bf2e183977d997299d94bc90eb03
淞謙 溫 <bnbn870829@gmail.com>
2021-07-19 17:28:48 +0800

    20210719

--- script.js
+++ script.js
@@ -2,10 +2,19 @@ $(document).ready(function(){
     AOS.init();
     $(window).scroll(function(){
         if(this.scrollY >  $(window).height()){
-            $('.navbar').addClass("sticky");
+            $('.navbar').addClass("sticky");   
         }
         else{
             $('.navbar').removeClass("sticky");
+        }
+    });
+
+    $(window).scroll(function(){
+        if(this.scrollY > 200){
+            $('.gotop').attr('style', 'opcaticy: 1');
+        }
+        else{
+            $('.gotop').attr('style', 'opacity: 0');
         }
     });
 
@@ -13,5 +22,10 @@ $(document).ready(function(){
     $('.menu-btn').click(function(){
         $('.navbar .navbar-nav').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
+    });
+
+    //go to top button
+    $('.gotop').click(function(){
+        $('html,body').animate({scrollTop: 0}, 500);
     });
 });
